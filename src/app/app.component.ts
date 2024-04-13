a92234bb8a019193b759ffaa0955cafe213e4343
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-13 13:28:55 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,22 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'piano';
+
+  PlaySound(nota: string) {
+    let audio = new Audio();
+    switch (nota) {
+      case 'DO':
+        audio.src = 'assets/audio/piano-DO.mp3';
+        break;
+        case 'RE':
+          audio.src='assets/audio/piano-RE.mp3'
+          break;
+    }
+    audio.load();
+    audio.play()
+  }
 }
