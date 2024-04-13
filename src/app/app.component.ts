145d93323850c74a8c000221f6b159e8f503cf29
Juliana Velandia <jvelandia@umariana.edu.co>
2024-04-13 13:48:54 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,26 @@ export class AppComponent {
       case 'DO':
         audio.src = 'assets/audio/piano-DO.mp3';
         break;
-        case 'RE':
-          audio.src='assets/audio/piano-RE.mp3'
-          break;
+      case 'RE':
+        audio.src = 'assets/audio/piano-RE.mp3';
+        break;
+      case 'MI':
+        audio.src = 'assets/audio/piano-MI.mp3';
+        break;
+      case 'FA':
+        audio.src = 'assets/audio/piano-FA.mp3';
+        break;
+      case 'SOL':
+        audio.src = 'assets/audio/piano-SOL.mp3';
+        break;
+      case 'LA':
+        audio.src = 'assets/audio/piano-LA.mp3';
+        break;
+      case 'SI':
+        audio.src = 'assets/audio/piano-SI.mp3';
+        break;
     }
     audio.load();
-    audio.play()
+    audio.play();
   }
 }
